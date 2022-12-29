a841623f27c3bcc5db7a8c744d8a31d687e06967
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-28 21:10:28 -0500

    Complete Add Toy Info to the Card

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 let addToy = false;
+const toysUrl = 'http://localhost:3000/toys'
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCardsConainer = document.querySelector("#toy-collection")
+console.log(toyCardsConainer)
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +15,28 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  const toys = fetch(toysUrl)
+    .then( res => res.json())
+    .then( data => {
+      console.log(data)
+      const cards = data.map( toy => {
+        const card = document.createElement('div')
+        card.className = 'card' 
+        card.innerHTML = `  <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes}</p>
+        <button class="like-btn" id=${toy.id}>Like ❤️</button>`
+        return card
+      })
+      return cards
+    })
+    .then( cards => { 
+      console.log(cards)
+      cards.forEach( card => {
+        toyCardsConainer.append(card)
+
+      })
+    })
+    .catch( err => console.error(err.message))
 });
