e9bc007079fc1a883bcfbe25ed5e007682f36b12
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-29 17:29:18 -0500

    POST request now correctly working

--- src/index.js
+++ src/index.js
@@ -6,53 +6,94 @@ document.addEventListener("DOMContentLoaded", () => {
   const toyFormContainer = document.querySelector(".container");
   const toyCardsConainer = document.querySelector("#toy-collection")
 
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
+
+      const form = document.querySelector('.add-toy-form')
+      form.addEventListener('submit', e => {
+        e.preventDefault()
+        // console.log('I was clicked')
+        const newToy = {
+          name: form.elements[0].value,
+          imageURL:  form.elements[1].value
+        }
+        // console.log( newToy )
+
+        postToy( newToy)
+      })
     } else {
       toyFormContainer.style.display = "none";
     }
+
+
   });
 
-  const getToys = fetch(toysUrl)
-    .then( res => res.json())
-    .then( data => {
-      const cards = data.map( toy => {
-        const card = document.createElement('div')
-        card.className = 'card' 
-        card.innerHTML = `  <h2>${toy.name}</h2>
-        <img src=${toy.image} class="toy-avatar" />
-        <p>${toy.likes}</p>
-        <button class="like-btn" id=${toy.id}>Like ❤️</button>`
-        return card
-      })
-      return cards
-    })
-    .then( cards => { 
-      cards.forEach( card => {
+  function renderCard(toy) {
+    const card = document.createElement('div')
+    card.className = 'card'
+    
+    card.innerHTML = `  <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes}</p>
+    <button class="like-btn" id=${toy.id}>Like ❤️</button>`
+
+    return card
+  }
+  function getToys() {
+    fetch(toysUrl)
+    .then(res => res.json())
+    .then(data => data.map(renderCard))
+        // const h2 = document.createElement('h2')
+        // const img = document.createElement('img')
+        // const p = document.createElement('p')
+        // const button = document.createElement('button')
+        // h2.textContent = toy.name
+        // img.src = toy.image
+        // img.className = 'toy-avatar'
+        // p.textContent = toy.likes
+        // button.className = 'like-btn'
+        // button.id = toy.id
+        // button.textContent = 'Like ❤️'
+        // card.append(h2)
+        // h2.append(img)
+        // img.append(p)
+        // p.append(button)
+
+        // add eventListener for click of like button
+    .then(cards => {
+      cards.forEach(card => {
         toyCardsConainer.append(card)
-
+        // console.log(card)
       })
     })
-    .catch( err => console.error(err.message))
-
-  const postToy = fetch(toysUrl, {
-    method: 'POST',
-    headers: {
-      "Content-Type" : "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify({
-      "name": "Jessie",
-      "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
-      "likes": 0
+    .catch(err => console.error(err.message))
+  }
+  function postToy(toy) {
+    fetch(toysUrl, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "name": toy.name,
+        "image": toy.imageURL,
+        "likes": 0
+      })
     })
-  })
-  .then( res => res.json())
-  .then( data => { 
-    console.log(data)
-  })
-  .catch( err => console.error(err.message))
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        const newToy = renderCard(data)
+        //append to DOM
+        toyCardsConainer.append(newToy)
+      })
+      .catch(err => console.error(err.message))
+  }
+
+  getToys()
 });
